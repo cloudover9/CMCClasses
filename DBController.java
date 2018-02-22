b5bf0f3881e3843327713bc515077a2326395041
IBUSH001 <ibush001@vplincsci019.ad.csbsju.edu>
2018-02-22 16:13:54 -0600

    Added in AccountInterface, AdminInterface, and DBController classes

---
+++ DBController.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+/*
+ * File:DBController.java
+ */
+
+/**
+ * Performs all the messy work needed for accessing and modifiying the databse
+ * @author Ian Bush
+ * @version February 22, 2018
+ */
+public class DBController
+{
+  /**
+   * Constructor
+   */
+  public DBController()
+  {
+  }
+  
+  /**
+   * Returns the list of all universities in the database
+   * @return all of the universities in the system
+   */
+  public ArrayList<String> getUniversities()
+  {
+    return null;
+  }
+  
+  /**
+   * Returns a list of all users in the databse
+   * @return all of the users in the system
+   */
+  public ArrayList<String> getAccounts()
+  {
+    return null;
+  }
+  
+  /**
+   * Finds the user in the database with the matching username
+   * return: the account with the matching username
+   * @param usr the username of the specific user
+   */
+  public void getUser(String usr)
+  {
+    //method is stated as void rather than Account, as Account has not been created yet
+  }
+  
+  /**
+   * 
+   */
+  public void addSchoolToUserList()
+  {
+  }
+}
