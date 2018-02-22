ec7a9d2ecc6b09061e1a161b62afa6dc1f83b7d6
NHUSSEIN001 <nhussein001@vplincsci045.ad.csbsju.edu>
2018-02-22 16:33:44 -0600

    Account Classes

---
+++ GeneralUser.java
@@ -0,0 +1,26 @@
+/** 
+  * File: GeneralUser.java
+  */
+
+import java.util.*; //Java's ArrayList<ET>
+//package cmcclasses
+
+/**
+ * GeneralUser is a class that is extends Account.
+ *
+ * 
+ * @author  Nimco Hussein
+ * @version February 22, 2018
+ */
+public class GeneralUser extends Account
+{
+  
+  /** Return the list of saved schools 
+    * @return ArrayList<String>   list of saved schools of a General User
+    */ 
+  public ArrayList<String> getSavedSchools()
+  {
+    return null;
+  }
+  
+}
