5f89df76d07816e4ce28b7f2de15983c62e47a9c
IBUSH001 <ibush001@vplincsci019.ad.csbsju.edu>
2018-02-22 16:29:11 -0600

    Modified AdminInterface and DBController classes

--- DBController.java
+++ DBController.java
@@ -21,7 +21,7 @@ public class DBController
    * Returns the list of all universities in the database
    * @return all of the universities in the system
    */
-  public ArrayList<String> getUniversities()
+  public ArrayList<University> getUniversities()
   {
     return null;
   }
@@ -46,9 +46,47 @@ public class DBController
   }
   
   /**
-   * 
+   * Adds the specified university to the school list of the specified user
+   * post: the University will be added to the user's school list
+   * @param usr the User whose list will now include the selected university
+   * @param univ the University which will be added to the User's school list
    */
-  public void addSchoolToUserList()
+  public void addSchoolToUserList(String usr, University univ)
+  {
+    //it will be a GeneralUser, not String
+  }
+  
+  /** 
+    * Adds a new university to the database
+    * post: a new University will be added to the database 
+    * @univ the University to add to the database
+    */
+  public void addNewUniversity(University univ)
+  {
+  }
+  
+  /**
+   * Adds a new account to the database
+   * post: a new Account will add to the database
+   * @param acc the account to add to the database
+   */
+  public void addAccount(String acc)
+  {
+  }
+  
+  /**
+   * Modifies an account
+   * @param acc the account to modify
+   */
+  public void updateAccount(String acc)
+  {
+  }
+  
+  /**
+   * Modifies a univeristy
+   * @param univ the University to modify
+   */
+  public void updateUniversity(University univ)
   {
   }
 }
