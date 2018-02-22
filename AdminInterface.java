0390b344a5d05ff675bf98472a8fdeb97021cdf4
IBUSH001 <ibush001@vplincsci019.ad.csbsju.edu>
2018-02-22 16:37:02 -0600

    Redo

---
+++ AdminInterface.java
@@ -0,0 +1,82 @@
+/*
+ * File:AdminInterface.java
+ */
+
+/**
+ * All the functionalities which the admin should be able to do
+ * @author Ian Bush
+ * @version February 22, 2018
+ */
+public class AdminInterface
+{
+  /**
+   * Constructor for the AdminInterface
+   */
+  public AdminInterface()
+  {
+  }
+  
+  /**
+   * Shows a list of all universities in the system
+   */
+  public void viewUniversities()
+  {
+  }
+  
+  /**
+   * Shows a list of all users in the system (both general and admin)
+   */
+  public void viewUsers()
+  {
+  }
+  
+  /**
+   * Modifies a university
+   * 
+   * @param univ The University to modify
+   */
+  public void editUniversity(University univ)
+  {
+   
+  }
+  
+  /**
+   * Adds a university to the database
+   */
+  public void addUniversity()
+  {
+  }
+  
+  /**
+   * Adds an account to the database
+   */
+  public void addAccount()
+  {
+  }
+  
+  /**
+   * Modifies a user
+   * @param user the user to modify
+   */
+  public void editUser()
+  {
+    //User not placed in the param section as the class has not been created yet
+  }
+  
+  /**
+   * Deactivates a user
+   * post: the user will be deactivated, and no longer able to access the system
+   * @param user the user to deactivate
+   */
+  public void deactivate()
+  {
+    //User not placed in the param section as the class has not been created yet
+  }
+  
+  /**
+   * Brings the admin to their homepage
+   */
+  public void homepage()
+  {
+  }
+}
