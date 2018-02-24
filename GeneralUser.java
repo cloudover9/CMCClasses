8b9aa3ed802d80bcbbd2e1622d0541c86562b777
NHUSSEIN001 <nhussein001@vplincsci045.ad.csbsju.edu>
2018-02-24 16:11:17 -0600

    Terminal 1: Modified files

--- GeneralUser.java
+++ GeneralUser.java
@@ -8,16 +8,32 @@ import java.util.*; //Java's ArrayList<ET>
 /**
  * GeneralUser is a class that is extends Account.
  *
- * 
  * @author  Nimco Hussein
  * @version February 22, 2018
  */
 public class GeneralUser extends Account
 {
+
   
-  /** Return the list of saved schools 
-    * @return ArrayList<String>   list of saved schools of a General User
-    */ 
+  /** A constructor that creates a GeneralUser object
+  * @param String firstName
+  * @param String  lastName
+  * @param Boolean  active
+  * @param Char     type
+  * @param String   password
+  */
+  public GeneralUser(String firstName, String lastName, boolean active,char type, String password)
+  {
+    super.setFirstName(firstName);
+    super.setLastName(lastName);
+    super.setActive(active);
+    super.setType(type);
+    super.setPassword(password);
+  }
+  
+  /** Returns the list of saved schools 
+  * @return ArrayList<String>   list of saved schools of a General User
+  */ 
   public ArrayList<String> getSavedSchools()
   {
     return null;
