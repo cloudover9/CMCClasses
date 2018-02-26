ecf88d01302f09beaae24f1f2600a859437ba8f0
IBUSH001 <ibush001@vplincsci066.ad.csbsju.edu>
2018-02-26 16:24:31 -0600

    Testing 1 2

--- CMCDriver.java
+++ CMCDriver.java
@@ -12,12 +12,6 @@ public class CMCDriver
 {
   public static void main(String [] args)
   {
-<<<<<<< HEAD
-    Scanner sc = new Scanner(System.in);
-    String username = sc.next("Enter a username");
-    String password = sc.next("Enter the password");
-    new AccountController().login(args[0], args[1]);
-=======
     System.out.println("Welcome to CMC");
     Scanner sc = new Scanner(System.in);
     System.out.println("Please enter your username");
@@ -25,6 +19,5 @@ public class CMCDriver
     System.out.println("Now enter your password");
     String p = sc.next();
     new AccountInterface().login(u, p);
->>>>>>> d28dc909ca353fa30e549b2624d394f18cf1c370
   }
 }
